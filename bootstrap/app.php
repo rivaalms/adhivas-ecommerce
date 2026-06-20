4355f37c78ecac0317e456bc38fb592a92777486
Riva Almero <rvalms@icloud.com>
2026-06-20 14:58:28 +0700

    refactor: remove Fortify integration and dismantle account settings modules

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,10 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
-
         $middleware->web(append: [
-            HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
